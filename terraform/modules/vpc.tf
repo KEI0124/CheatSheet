37ca0c6617c8d8ed0a0736590c3622eb3d4a9190
ksaito-hub <saito@tec-p.co.jp>
2021-07-30 11:30:24 +0900

    push

--- terraform/modules/vpc.tf
+++ terraform/modules/vpc.tf
@@ -2,6 +2,6 @@ resource "aws_vpc" "this" {
   cidr_block           = "10.1.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = false
-  instance_tenancy     = default
+  instance_tenancy     = "default"
   tags = { "Name" = "VPC" }
 }
